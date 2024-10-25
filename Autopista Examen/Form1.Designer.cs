27a1f234ac0dffc2bf88bc96a9148711ff8b8b86
Admin <Admin@RAMOS>
2024-10-24 20:26:28 -0500

    Agregar archivos de proyecto.

---
+++ Autopista Examen/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Autopista_Examen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.picCarro = new System.Windows.Forms.PictureBox();
+            this.pictureBoxObstaculo = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCarro)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxObstaculo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Controls.Add(this.pictureBoxObstaculo);
+            this.panel1.Controls.Add(this.picCarro);
+            this.panel1.Controls.Add(this.pictureBox6);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(0, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(381, 556);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(173, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(14, 224);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox2.Location = new System.Drawing.Point(173, 275);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(14, 222);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox6.Location = new System.Drawing.Point(173, 548);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(14, 120);
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            // 
+            // picCarro
+            // 
+            this.picCarro.BackColor = System.Drawing.Color.Transparent;
+            this.picCarro.BackgroundImage = global::Autopista_Examen.Properties.Resources.Carro_1;
+            this.picCarro.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picCarro.Location = new System.Drawing.Point(22, 422);
+            this.picCarro.Name = "picCarro";
+            this.picCarro.Size = new System.Drawing.Size(111, 116);
+            this.picCarro.TabIndex = 6;
+            this.picCarro.TabStop = false;
+            // 
+            // pictureBoxObstaculo
+            // 
+            this.pictureBoxObstaculo.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxObstaculo.BackgroundImage = global::Autopista_Examen.Properties.Resources.Carro_2;
+            this.pictureBoxObstaculo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBoxObstaculo.Location = new System.Drawing.Point(235, 13);
+            this.pictureBoxObstaculo.Name = "pictureBoxObstaculo";
+            this.pictureBoxObstaculo.Size = new System.Drawing.Size(117, 129);
+            this.pictureBoxObstaculo.TabIndex = 7;
+            this.pictureBoxObstaculo.TabStop = false;
+            
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 553);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCarro)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxObstaculo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox picCarro;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBoxObstaculo;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Autopista Examen/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Autopista_Examen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown); // Enlazar el evento KeyDown
+            this.KeyPreview = true;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Focus();  // Forzar el enfoque al cargar el formulario
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int velocidad = 10; // Velocidad de movimiento del carro
+
+            // Mover a la izquierda
+            if (e.KeyCode == Keys.Left)
+            {
+                if (picCarro.Left > 0) // Limitar para que no salga del borde izquierdo
+                {
+                    picCarro.Left -= velocidad;
+                }
+            }
+
+            // Mover a la derecha
+            if (e.KeyCode == Keys.Right)
+            {
+                if (picCarro.Right < panel1.Width) // Limitar para que no salga del borde derecho
+                {
+                    picCarro.Left += velocidad;
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Mover las líneas de la carretera
+            pictureBox1.Top += 5;
+            pictureBox2.Top += 5;
+
+            // Recolocar las líneas si salen de la pantalla
+            if (pictureBox1.Top >= panel1.Height)
+                pictureBox1.Top = -pictureBox1.Height;
+
+            if (pictureBox2.Top >= panel1.Height)
+                pictureBox2.Top = -pictureBox2.Height;
+
+            // Mover el obstáculo
+            moverObstaculo();
+
+            // Detectar colisiones
+            detectarColision();
+        }
+
+        private void moverObstaculo()
+        {
+            int velocidadObstaculo = 8; // Velocidad del obstáculo
+
+            // Mover el obstáculo hacia abajo
+            pictureBoxObstaculo.Top += velocidadObstaculo;
+
+            // Si el obstáculo sale del panel, recolocarlo en la parte superior
+            if (pictureBoxObstaculo.Top >= panel1.Height)
+            {
+                Random rand = new Random();
+                pictureBoxObstaculo.Top = -pictureBoxObstaculo.Height; // Recolocar en la parte superior
+                pictureBoxObstaculo.Left = rand.Next(0, panel1.Width - pictureBoxObstaculo.Width); // Posición aleatoria en la carretera
+            }
+        }
+
+        private void detectarColision()
+        {
+            // Si el carro colisiona con el obstáculo, detener el juego
+            if (picCarro.Bounds.IntersectsWith(pictureBoxObstaculo.Bounds))
+            {
+                timer1.Stop();
+                MessageBox.Show("¡Has chocado! Fin del juego.");
+            }
+        }
+    }
+}
